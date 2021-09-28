f8c94d2a8432c4f444f7457227de74edf1015ef5
atif shafi <atifshafi63@gmail.com>
2021-09-28 17:25:53 +0530

    environ installation

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-from .secret import *
+#from .secret import *
 import dj_database_url
 import environ
 
@@ -26,7 +26,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.secret'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'jobportal/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
